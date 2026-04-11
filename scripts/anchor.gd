989ed0ec573a11603d6dc74cd44da33db18cd4f8
TwiginAround156 <devink2010@outlook.com>
2026-04-11 12:52:18 -0600

    made a spawnrate timer and fixed a glitch

--- scripts/anchor.gd
+++ scripts/anchor.gd
@@ -7,6 +7,7 @@ extends Node2D
 @onready var projectile_list: Node = $Projectiles
 @onready var projectile_spawn_pos: Marker2D = $Weapon/ProjectileSpawn
 @onready var spitball_scene: PackedScene = preload("res://scenes/spit_ball.tscn")
+var can_shoot := true
 
 var default_weapon_place = Vector2.RIGHT
 
@@ -16,8 +17,11 @@ func _process(_delta: float) -> void:
 	rotation = angle # Gives the anchor the angle
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") and can_shoot == true:
 		spawn_spitball()
+		can_shoot = false
+		await get_tree().create_timer(0.2).timeout
+		can_shoot = true
 
 func spawn_spitball() -> void:
 	var mouse_position := get_global_mouse_position() # Get the mouse position
