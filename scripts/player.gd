58412545bd71ff75569f629bdd2e25de92881c16
TwiginAround156 <devink2010@outlook.com>
2026-04-10 21:28:08 -0600

    added basic player with no sprite + player movement

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+@export var speed = 10000.0
+
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var _directionx := Input.get_axis("move_left", "move_right")
+	var _directiony := Input.get_axis("move_up", "move_down")
+	if _directionx:
+		velocity.x = _directionx * speed * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	if _directiony:
+		velocity.y = _directiony * speed * delta
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+
+	move_and_slide()
