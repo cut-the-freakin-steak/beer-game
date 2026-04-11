51c8d8572cc4a603c5cef7b24725d2e4e05a6a8c
cut-the-freakin-steak <calvaradolopez@proton.me>
2026-04-11 09:31:06 -0600

    updated a lot of the project settings and fixed some uids

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,6 @@ extends CharacterBody2D
 
 
 func _physics_process(delta: float) -> void:
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var _directionx := Input.get_axis("move_left", "move_right")
