a345a1ae5ae96ab3897fee8a5c6df42ded345244
cut-the-freakin-steak <calvaradolopez@proton.me>
2026-04-11 16:58:49 -0600

    z-indexing is good for brewing minigame, but collision detection is still broken

--- scripts/brewing_minigame.gd
+++ scripts/brewing_minigame.gd
@@ -20,18 +20,26 @@ var last_mouse_position: Vector2 = Vector2.ZERO
 var resting_position: Vector2 = Vector2(0, 0)
 
 func _ready() -> void:
-	# devin, if you're seeing this is just a way to connect signals that isn't in the editor
-	# i like it like this because you can see all of the signals in the code
-	# instead of having to look in the editor
-	# you dont have to do this if you don't want to
-	# pot_main_area.mouse_entered.connect(_mouse_entered_shape.bind(pot_main_area))
+	# i love groups. you can mark certain nodes and then just do stuff on those nodes
 	for node in get_tree().get_nodes_in_group("BrewingMouseInteractables"):
-		print(node)
+		# redefining the loop variable cause its a Node by default and we need it to be an
+		# Area2D to get the signals in LSP
 		var area: Area2D = node
+
+		# devin, if you're seeing this is just a way to connect signals that isn't in the editor
+		# i like it like this because you can see all of the signals in the code
+		# instead of having to look in the editor
+		# you dont have to do this if you don't want to
 		area.mouse_entered.connect(_mouse_entered_shape.bind(area))
-		area.mouse_exited.connect(_mouse_exited_shape)
+		area.mouse_exited.connect(_mouse_exited_shape.bind(area))
+	
+	burner_area.area_entered.connect(_burner_area_entered)
 
 func _process(delta: float):
+	if not Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT):
+		if gonna_mouse_collide and collision_target.get_ref() == null:
+			gonna_mouse_collide = false
+
 	if gonna_mouse_collide:
 		var mouse_position = get_global_mouse_position()
 		last_mouse_position = mouse_position
@@ -45,11 +53,35 @@ func _process(delta: float):
 		else:
 			collision_body.global_position = lerp(collision_body.global_position, resting_position, 0.1)
 
+	# pot bottom area burner area
+
 func _mouse_entered_shape(body: Area2D) -> void:
+	if Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT):
+		return
+
 	gonna_mouse_collide = true
 	resting_position = body.global_position
 	collision_target = weakref(body)
 
-func _mouse_exited_shape() -> void:
-	if not Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT):
-		gonna_mouse_collide = false
+	var body_parent = body.get_parent()
+	body_parent.visibility_layer = 2
+	body_parent.top_level = true
+	body_parent.z_index = 1
+
+func _mouse_exited_shape(body: Area2D) -> void:
+	if Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT):
+		return
+
+	gonna_mouse_collide = false
+
+	var body_parent = body.get_parent()
+	body_parent.visibility_layer = 1
+	body_parent.top_level = false
+	body_parent.z_index = 0
+
+func _burner_area_entered(body: Area2D):
+	if Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT):
+		return
+
+	if body == pot_bottom_area:
+		print("heyo")
