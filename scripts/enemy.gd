39a0b30e1f0ad7041916789b9ffa95492b433073
TwiginAround156 <devink2010@outlook.com>
2026-04-12 10:11:01 -0600

    Very basic enemy ai.l health bar(WIP)

---
+++ scripts/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+class_name Enemy
+
+@export var speed: float = 87.0
+@export var ranged: bool = false
+@export var damage: int = 20
+@onready var Player = $"../Player"
+
+func _physics_process(delta: float) -> void:
+	if ranged == false:
+		var direction = Player.position - position
+		velocity = direction * speed * delta
+		
+
+	move_and_slide()
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if ranged == true:
+		return
+	else:
+		$"../Player/HUD/HealthBar".value -= damage
