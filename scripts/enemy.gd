6be6eacf3176cf594afbe6d9b71d2c240d62290e
TwiginAround156 <devink2010@outlook.com>
2026-04-13 17:01:21 -0600

    Straw sprite added | cop ai closer to finished

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,27 +1,35 @@
 extends CharacterBody2D
 class_name Enemy
 
-@export var speed: float = 87.0
+@export var speed: float = 1750.0
 @export var ranged: bool = false
 @export var damage: int = 20
 @export var health: int = 5
 @onready var Player: CharacterBody2D = $"../Player"
 @onready var PlayerHurtbox: Area2D = $"../Player/HurtBox"
 @onready var PlayerHealth: ProgressBar = $"../Player/HUD/HealthBar"
+@onready var spitball_scene: PackedScene = preload("res://scenes/spit_ball.tscn")
+@onready var BulletSpawner: Marker2D = $Anchor/Gun/BulletSpawner
+var wanted_distance = 90
 
 func _physics_process(delta: float) -> void:
 	if ranged == false:
-		var direction = Player.position - position
+		var direction = global_position.direction_to(Player.global_position)
 		velocity = direction * speed * delta
 	else:
-		if $RangeToPlayer.overlaps_area(PlayerHurtbox):
-			pass
-		else:
-			var direction = Player.position - position
+		var dis = global_position.distance_to(Player.global_position)
+		var direction = global_position.direction_to(Player.global_position)
+		if dis > wanted_distance + 10:
 			velocity = direction * speed * delta
+		elif dis < wanted_distance + 10:
+			velocity = -direction * speed * delta
+		else:
+			velocity = Vector2.ZERO
 		var player_position = $"../Player".global_position
 		var angle := global_position.angle_to_point(player_position)
 		$Anchor.rotation = angle
+		if $PlayerRange.overlaps_area(PlayerHurtbox):
+			spawn_bullet()
 
 	move_and_slide()
 
@@ -31,7 +39,8 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 		$"../Player/HurtSound".play()
 		PlayerHealth.value -= damage
 
-func _on_range_to_player_area_entered(area: Area2D) -> void:
-	if area == PlayerHurtbox:
-		var direction = Player.position - position
-		velocity = -direction * speed
+func spawn_bullet() -> void:
+	var direction = global_position.direction_to(Player.global_position)
+	var spitball: Projectile = spitball_scene.instantiate()
+	spitball.global_position = BulletSpawner.global_position
+	spitball.velocity = spitball.speed * direction
--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -4,6 +4,8 @@ class_name Projectile
 @export var speed: float = 300.0
 @onready var ZombieHitbox: Area2D = $"../../../../Zombie/Hitbox"
 @onready var Zombie: CharacterBody2D = $"../../../../Zombie"
+@onready var CopHitbox: Area2D = $"../../../../Cop/Hitbox"
+@onready var Cop: CharacterBody2D = $"../../../../Cop"
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
@@ -14,3 +16,8 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 		queue_free()
 		if Zombie.health == 0:
 			Zombie.queue_free()
+	elif area == CopHitbox:
+		Cop.health -= 1
+		queue_free()
+		if Cop.health == 0:
+			Cop.queue_free()
