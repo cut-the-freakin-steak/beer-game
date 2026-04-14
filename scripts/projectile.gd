9829c52e8a6fbb0521582bb6ee6b648dae3e3e3c
TwiginAround156 <devink2010@outlook.com>
2026-04-14 17:03:55 -0600

    IDK what the issue is

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Projectile
 
 @export var speed: float = 300.0
+@export var damage = 1
 @onready var ZombieHitbox: Area2D = $"../../../../Zombie/Hitbox"
 @onready var Zombie: CharacterBody2D = $"../../../../Zombie"
 @onready var CopHitbox: Area2D = $"../../../../Cop/Hitbox"
@@ -12,12 +13,12 @@ func _physics_process(_delta: float) -> void:
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area == ZombieHitbox:
-		Zombie.health -= 1
+		Zombie.health -= damage
 		queue_free()
 		if Zombie.health == 0:
 			Zombie.queue_free()
 	elif area == CopHitbox:
-		Cop.health -= 1
+		Cop.health -= damage
 		queue_free()
 		if Cop.health == 0:
 			Cop.queue_free()
--- scripts/weapons.gd
+++ scripts/weapons.gd
@@ -3,25 +3,37 @@ class_name Weapons
 
 signal new_weapon
 
+@export var damage = 2
 @onready var Player: CharacterBody2D = $"../Player"
 @onready var PlayerHurtbox: Area2D = $"../Player/HurtBox"
 @onready var WeaponDefault: Marker2D = $"../Player/Anchor/WeaponDefault"
 @onready var BeerWeaponScene: PackedScene = preload("res://scenes/beer_bottle_weapon.tscn")
+@onready var ZombieHitbox: Area2D = $"../../../Zombie/Hitbox"
+@onready var Zombie: CharacterBody2D = $"../../../Zombie"
+@onready var CopHitbox: Area2D = $"../../../Cop/Hitbox"
+@onready var Cop: CharacterBody2D = $"../../../Cop"
 var can_shoot := true
 var has_weapon: bool = false
 var ran: bool = false
 var default_weapon_place = Vector2.RIGHT
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if has_weapon == true:
 		var mouse_position := get_global_mouse_position()
 		var angle := global_position.angle_to_point(mouse_position)
 		rotation = angle
-		if Input.is_action_pressed("spitball"):
+		if Input.is_action_just_pressed("spitball"):
 			hide()
-		elif Input.is_action_pressed("weapon2"):
+		if Input.is_action_just_pressed("weapon2"):
 			show()
+			if Input.is_action_pressed("shoot"):
+				if $Range.is_colliding():
+					var enemy = $Range.get_collider()
+					print(enemy)
+					enemy.health -= damage
+					queue_free()
+					if enemy.health == 0:
+						enemy.queue_free()
 
 	if ran == false:
 		if $PickUpRange.overlaps_area(PlayerHurtbox) == false:
@@ -31,8 +43,7 @@ func _process(delta: float) -> void:
 			if Input.is_action_pressed("pick_up"):
 				Player.has_weapon = true
 				has_weapon = true
-				$PickUpRange.monitoring = false
-				$PickUpRange.monitorable = false
+				$PickUpRange.queue_free()
 				queue_free()
 	if has_weapon == true and ran == false:
 		var BeerWeapon: Weapons = BeerWeaponScene.instantiate()
